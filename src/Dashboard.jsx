a431da62ebf903a347662ddcd345c852e556f51c
AjithSimson <ajithsimson@gmail.com>
2024-08-13 18:56:13 +0530

    logic change

--- src/Dashboard.jsx
+++ src/Dashboard.jsx
@@ -6,45 +6,26 @@ import Popup from './popUp'
 const Dashboard = () => {
   const [popup, setPopup] = useState(null);
   const [message, setMessage] = useState('');
-  const [cancelClicked, setCancelClicked] = useState(false);
-
+  
   const handleShowMessage = (msg) => {
     setMessage(msg);
   };
-
-const handleCancelClick = ()=>{
-  setPopup(true);
-}
-
-
-
-
-
 
   const showConfirm = () => {
     setPopup({
       type: 'confirm',
       message: 'Are you sure?',
-    });
-    
-  };
-   
-
-  const handleConfirm = () => {
-   setCancelClicked(true);
-   setPopup(false)
-    // alert('Okey has been Clicked');
-    // setPopup(null);
+    });  
   };
 
- 
- 
-
-
-  const closePopup = () => {
+  const handlePopBtnClick = (isYesClicked=false) => {
+    if(isYesClicked) {
+      setMessage("cancel button is clicked");
+    }else{
+      setMessage("");
+    }
     setPopup(null);
-   
-  };
+  }
 
   return (
     <div>
@@ -66,16 +47,15 @@ const handleCancelClick = ()=>{
       
       {message && <p>{message}</p>}
        
-       <CancelBtn  onCancel={showConfirm} />
+      <CancelBtn  onCancel={showConfirm} />
       
       </div>
       {popup && (
         <Popup
           type={popup.type}
           message={popup.message}
-          onClose={popup.type === 'alert' ? closePopup : null}
-          onConfirm={popup.type === 'confirm' ? closePopup : null}
-          
+          onCancel={() => handlePopBtnClick(false) }
+          onConfirm={() => handlePopBtnClick(true) }
         />
       )}
     </div>
